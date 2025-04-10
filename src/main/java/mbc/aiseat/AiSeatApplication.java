225808922d17c664b1cce1e3c0b6e94495c81d89
312-05\gidtjrwns <gidtjrwns30@naver.com>
2025-04-10 11:31:09 +0900

    Initial commit

---
+++ src/main/java/mbc/aiseat/AiSeatApplication.java
@@ -0,0 +1,13 @@
+package mbc.aiseat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiSeatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiSeatApplication.class, args);
+    }
+
+}
